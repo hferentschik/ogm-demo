519ee69acb88e05e73d21472da258a9b6fdf9bb6
Hardy Ferentschik <hardy@ferentschik.de>
2011-05-31 15:25:39 +0200

    Starting cache manager as part of the test

--- src/test/java/org/hibernate/jpa/demo/InsertAndQueryTest.java
+++ src/test/java/org/hibernate/jpa/demo/InsertAndQueryTest.java
@@ -25,15 +25,27 @@ package org.hibernate.jpa.demo;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.transaction.TransactionManager;
 
 import junit.framework.TestCase;
 import org.apache.lucene.search.Query;
+import org.infinispan.Cache;
+import org.infinispan.config.Configuration;
+import org.infinispan.config.ConfigurationValidatingVisitor;
+import org.infinispan.config.GlobalConfiguration;
+import org.infinispan.config.InfinispanConfiguration;
+import org.infinispan.manager.CacheContainer;
+import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.manager.EmbeddedCacheManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.hibernate.cfg.Environment;
+import org.hibernate.ogm.datastore.infinispan.impl.TransactionManagerLookupDelegator;
 import org.hibernate.search.jpa.FullTextEntityManager;
 import org.hibernate.search.jpa.FullTextQuery;
 import org.hibernate.search.jpa.Search;
@@ -43,17 +55,20 @@ import org.hibernate.search.query.dsl.QueryBuilder;
 import org.hibernate.transaction.JBossTSStandaloneTransactionManagerLookup;
 
 public class InsertAndQueryTest extends TestCase {
-	private EntityManagerFactory entityManagerFactory;
-	FullTextEntityManager fullTextEntityManager;
 	private static final Logger log = LoggerFactory.getLogger( InsertAndQueryTest.class );
+
+	private FullTextEntityManager fullTextEntityManager;
+	private CacheContainer cacheContainer;
 
 	@Override
 	protected void setUp() throws Exception {
+		cacheContainer = createCustomCacheManager( "infinispan-config.xml", new Properties() );
+
 		TransactionManager transactionManager = new JBossTSStandaloneTransactionManagerLookup().getTransactionManager(
 				null
 		);
 		transactionManager.begin();
-		entityManagerFactory = Persistence.createEntityManagerFactory( "ogm-demo" );
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory( "ogm-demo" );
 		fullTextEntityManager = Search.getFullTextEntityManager( entityManagerFactory.createEntityManager() );
 		createTestData();
 	}
@@ -75,6 +90,11 @@ public class InsertAndQueryTest extends TestCase {
 		List<Event> result = fullTextQuery.getResultList();
 		for ( Event event : result ) {
 			log.info( event.toString() );
+		}
+
+		Cache<String, Object> cache = cacheContainer.getCache( "ENTITIES" );
+		for ( Map.Entry entry : cache.entrySet() ) {
+			log.info( entry.getKey().toString() + "" + entry.getValue().toString() );
 		}
 
 		fullTextEntityManager.getTransaction().commit();
@@ -114,4 +134,39 @@ public class InsertAndQueryTest extends TestCase {
 		}
 		fullTextEntityManager.getTransaction().commit();
 	}
+
+	static EmbeddedCacheManager createCustomCacheManager(String cfgName, Properties properties) {
+		try {
+			InfinispanConfiguration configuration = InfinispanConfiguration.newInfinispanConfiguration(
+					cfgName, InfinispanConfiguration.resolveSchemaPath(),
+					new ConfigurationValidatingVisitor()
+			);
+			GlobalConfiguration globalConfiguration = configuration.parseGlobalConfiguration();
+			Configuration defaultConfiguration = configuration.parseDefaultConfiguration();
+			properties.setProperty(
+					Environment.TRANSACTION_MANAGER_STRATEGY,
+					JBossTSStandaloneTransactionManagerLookup.class.getName()
+			);
+			TransactionManagerLookupDelegator transactionManagerLookupDelegator = new TransactionManagerLookupDelegator(
+					properties
+			);
+			final DefaultCacheManager cacheManager = new DefaultCacheManager(
+					globalConfiguration,
+					defaultConfiguration,
+					true
+			);
+			for ( Map.Entry<String, Configuration> entry : configuration.parseNamedConfigurations().entrySet() ) {
+				Configuration cfg = entry.getValue();
+				cfg.setTransactionManagerLookup( transactionManagerLookupDelegator );
+				cacheManager.defineConfiguration( entry.getKey(), cfg );
+			}
+			return cacheManager;
+		}
+		catch ( Exception e ) {
+			e.printStackTrace();
+			System.exit( -1 );
+		}
+		return null; //actually this line is unreachable
+	}
+
 }
